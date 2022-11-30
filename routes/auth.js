5b89ebc0806a382cedc4c9161d635242b1f9b644
Abdeali <abdealichharchhoda786@gmail.com>
2022-11-30 11:29:17 +0530

    added bcrypt and JWT

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const User = require('../models/modUser');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
+// <-- JWT secret --> //
+const JWT_Secret = "Abdeali@786#123";
 
 // <-- validation array --> //
-
 const validationArray = [
     body('name', 'Enter valid name it must be minmum length of 3.').isLength({ min: 3 }),
     body('email', 'Enter valid Email.').isEmail(),
@@ -35,14 +40,34 @@ router.post('/createUser', validationArray, async (req, res) => {
             return res.status(400).json({ error: "User already exists." })
         }
 
+        // <-- Securing Password --> //
+
+        const salt = await bcrypt.genSalt(10); // -> generate salts. (10 represent)
+
+        // console.log("salt is  : ",salt);
+
+        const securedPassword = await bcrypt.hash(req.body.password,salt);
+
+        // console.log("password is  : ",securedPassword);
+
         /* Creating user using mongoose model */
         let newUser = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: securedPassword
         })
 
-        res.json(newUser);
+        /* <-- generating token --> */
+
+        const dataToSend={
+            newUserId : newUser.id
+        }
+
+        const authToken = jwt.sign(dataToSend,JWT_Secret); // -> return promise (sync function)
+
+        // console.log(authToken);        
+
+        res.json({authToken});
 
     } catch (error) {
 
